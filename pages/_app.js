d9d532dd3413656805ac64de057c776311899be6
TheSmartBeaver <totorx36bhu@gmail.com>
2021-07-12 15:43:29 +0200

    added page tracking for google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,20 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
       analytics()
+
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url)
+        analytics().logEvent("screen_view")
+      }
+
+      routers.events.on("routeChangeComplete", logEvent)
+      //For First Page
+      logEvent(window.location.pathname)
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off("routeChangeComplete", logEvent)
+      }
     }
   }, [])
 
